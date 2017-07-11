efe5bb92790924d11f1cee2e28544914ad3e4e43
Tom Almeida <tommoa256@gmail.com>
2017-07-12 04:26:51 +0800

    Swapped error handling to use a switch statement and added case for type_not_found

--- src/errors.cpp
+++ src/errors.cpp
@@ -6,11 +6,24 @@ void errors::dispatcher(std::ostream& out) {
 	try {
 		throw;
 	} catch (types T) {
-		if (T == types::invalid_set)
-			out << "Invalid entry" << std::endl;
-		if (T == types::invalid_file_type)
-			out << "Invalid file type. Allowed types are 'file' and 'binary'"
-				<< std::endl;
+		switch (T) {
+			case types::invalid_set:
+				out << "Invalid entry" << std::endl;
+				break;
+			case types::invalid_file_type:
+				out << "Invalid file type. Allowed types are 'file' and "
+					   "'binary'"
+					<< std::endl;
+				break;
+			case types::type_not_found:
+				out << "Either type is not found or the type was not set "
+					   "successfully"
+					<< std::endl;
+				break;
+			default:
+				out << "Unknown error" << std::endl;
+				break;
+		}
 	} catch (std::exception e) {
 		out << "Error!\n\t" << e.what() << std::endl;
 	}
