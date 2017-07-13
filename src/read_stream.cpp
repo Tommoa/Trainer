6dd04e7dac105d7ee63138b1577839bd0d14fb8e
Tom Almeida <tommoa256@gmail.com>
2017-07-14 06:05:24 +0800

    Moved all sub functions of read_stream out of the main function. They will be later moved into their own files

--- src/read_stream.cpp
+++ src/read_stream.cpp
@@ -3,128 +3,167 @@
 #include "data_member.hpp"
 #include "errors.hpp"
 
+#include "Snippets/defer.hpp"
+
 #include <fstream>
 #include <sstream>
 #include <string>
+
+void create_variable(std::vector<data_member>& data_member_list,
+					 std::istream& ss) {
+	data_member temp;
+
+	std::string type;
+	if (!(ss >> type))
+		throw errors::types::incomplete_command;
+	temp.str_to_type_id(type);
+
+	std::string name;
+	if (!(ss >> name))
+		throw errors::types::incomplete_command;
+	temp.name = name;
+
+	size_t offset;
+	while ((ss >> std::hex >> offset)) {
+		temp.offsets.push_back(std::move(offset));
+	}
+
+	data_member_list.push_back(std::move(temp));
+}
+
+void set_variable(std::vector<data_member>& data_member_list,
+				  std::istream& ss) {
+	std::string name;
+	if (!(ss >> name))
+		throw errors::types::incomplete_command;
+
+	std::string to;
+	if (!(ss >> to))
+		throw errors::types::incomplete_command;
+
+	bool found = false;
+	for (auto& member : data_member_list)
+		if (member.name == name) {
+			member.set_data(to);
+			found = true;
+			break;
+		}
+
+	if (!found)
+		throw errors::types::not_a_variable;
+}
+
+void get_variable(std::vector<data_member>& data_member_list, std::istream& ss,
+				  std::ostream& out) {
+	std::string name;
+	if (!(ss >> name))
+		throw errors::types::incomplete_command;
+
+	bool found = false;
+	for (auto& member : data_member_list)
+		if (member.name == name) {
+			out << member.get_data() << std::endl;
+			found = true;
+			break;
+		}
+	if (!found)
+		throw errors::types::not_a_variable;
+}
+
+void export_file(std::vector<data_member>& data_member_list, std::istream& in) {
+	std::string file_format;
+	if (!(in >> file_format))
+		throw errors::types::incomplete_command;
+
+	std::string filename;
+	if (!(in >> file_format))
+		throw errors::types::incomplete_command;
+
+	if (file_format != "binary" && file_format != "file") {
+		throw errors::types::invalid_file_type;
+	}
+
+	std::ofstream file(
+		filename, file_format == "binary" ? std::ios::binary : std::ios::trunc);
+
+	defer { file.close(); }; // Ensure we close the file regardless of errors
+
+	for (auto& member : data_member_list) {
+		file << "create " << member.get_type() << " " << member.name;
+		file << std::endl;
+		file << "set " << member.name << " " << member.get_data() << std::endl;
+	}
+}
+
+void import_file(std::vector<data_member>& data_member_list, std::istream& in,
+				 std::ostream& out, std::ostream& err) {
+	std::string mode;
+	if (!(in >> mode))
+		throw errors::types::incomplete_command;
+
+	std::string filename;
+	if (!(in >> filename))
+		throw errors::types::incomplete_command;
+
+	if (mode != "binary" && mode != "file") {
+		throw errors::types::invalid_file_type;
+	}
+	std::ifstream file(filename,
+					   mode == "binary" ? std::ios::binary : std::ios::in);
+	defer { file.close(); };
+	read_stream(data_member_list, file, out, err);
+}
 
 void read_stream(std::vector<data_member>& data_member_list, std::istream& in,
 				 std::ostream& out, std::ostream& err) {
 	std::string line;
 	while (true) {
 		std::getline(in, line);
-		int last = 0;
-		int current = line.find(' ', last);
-		if (current == 0)
-			break;
-		if (line.substr(last, current - last) == "create") {
-			last = current + 1;
+		std::istringstream ss(line);
+		std::string command;
+		if (!(ss >> command)) // Empty line, just continue.
+			continue;
+		if (command == "create") {
 			try {
-				current = line.find(' ', last);
-				data_member temp;
-				temp.str_to_type_id(line.substr(last, current - last));
-				last = current + 1;
-				current = line.find(' ', last);
-				temp.name = line.substr(last, current - last);
-				last = current + 1;
-				std::istringstream ss(line.substr(last, line.size()));
-				size_t offset;
-				while ((ss >> std::hex >> offset)) {
-					temp.offsets.push_back(std::move(offset));
-				}
-				data_member_list.push_back(std::move(temp));
+				create_variable(data_member_list, ss);
 			} catch (...) {
 				errors::dispatcher(err);
 			}
 			continue;
 		}
-		if (line.substr(last, current - last) == "set") {
-			last = current + 1;
+		if (command == "set") {
 			try {
-				current = line.find(' ', last);
-				auto n = line.substr(last, current - last);
-				last = current + 1;
-				bool found = false;
-				for (auto& member : data_member_list) {
-					if (member.name == n) {
-						member.set_data(line.substr(last, line.size()));
-						found = true;
-						break;
-					}
-				}
-				if (!found)
-					throw errors::types::not_a_variable;
+				set_variable(data_member_list, ss);
 			} catch (...) {
 				errors::dispatcher(err);
 			}
 			continue;
 		}
-		if (line.substr(last, current - last) == "get") {
-			last = current + 1;
+		if (command == "get") {
 			try {
-				current = line.find(' ', last);
-				auto n = line.substr(last, current - last);
-				last = current + 1;
-				bool found = false;
-				for (auto& member : data_member_list) {
-					if (member.name == n) {
-						out << member.get_data() << std::endl;
-						found = true;
-						break;
-					}
-				}
-				if (!found)
-					throw errors::types::not_a_variable;
+				get_variable(data_member_list, ss, out);
 			} catch (...) {
 				errors::dispatcher(err);
 			}
 			continue;
 		}
-		if (line.substr(last, current) == "export") {
+		if (command == "export") {
 			try {
-				last = current + 1;
-				current = line.find(' ', last);
-				auto type = line.substr(last, current - last);
-				last = current + 1;
-				if (type != "binary" && type != "file") {
-					throw errors::types::invalid_file_type;
-				}
-				auto filename = line.substr(last, line.size());
-				std::ofstream file(filename,
-								   type == "binary" ? std::ios::binary
-													: std::ios::trunc);
-				for (auto member : data_member_list) {
-					file << "create " << member.get_type() << " "
-						 << member.name;
-					file << std::endl;
-					file << "set " << member.name << " " << member.get_data()
-						 << std::endl;
-				}
-				file.close();
+				export_file(data_member_list, ss);
 			} catch (...) {
 				errors::dispatcher(err);
 			}
 			continue;
 		}
-		if (line.substr(last, current) == "import") {
+		if (command == "import") {
 			try {
-				last = current + 1;
-				current = line.find(" ", last);
-				auto mode = line.substr(last, current - last);
-				if (mode != "binary" && mode != "file") {
-					throw errors::types::invalid_file_type;
-				}
-				last = line.find(" ", current + 1) + 1;
-				auto filename = line.substr(last, line.size());
-				std::ifstream file(filename,
-								   mode == "binary" ? std::ios::binary
-													: std::ios::in);
-				read_stream(data_member_list, file, out, err);
+				import_file(data_member_list, ss, out, err);
 			} catch (...) {
 				errors::dispatcher(err);
 			}
 			continue;
 		}
-		if (line.substr(last, current) == "exit")
+		if (command == "exit")
 			break;
 		try {
 			throw errors::types::not_a_command;
