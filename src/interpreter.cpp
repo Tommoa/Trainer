2b15b2d1cc53dcd402fc47d9d2e83cf232596a58
Tommoa <tommoa256@gmail.com>
2017-07-23 16:24:40 +0800

    Added two additonal commands, list and help

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -18,6 +18,8 @@ const std::wstring get_variable_command = L"get";
 const std::wstring export_variables_command = L"export";
 const std::wstring import_variables_command = L"import";
 const std::wstring exit_command = L"exit";
+const std::wstring help_command = L"help";
+const std::wstring list_variables_command = L"list";
 #else
 const std::string new_executable_command = "[";
 const std::string window_command = "[[";
@@ -26,7 +28,9 @@ const std::string set_variable_command = "set";
 const std::string get_variable_command = "get";
 const std::string export_variables_command = "export";
 const std::string import_variables_command = "import";
-const std::string exit_command = "exit";
+const std::string exit_command = "exit"; 
+const std::string help_command = "help";
+const std::string list_variables_command = "list";
 #endif
 
 bool interpreter::read_stream(std::vector<data_member>& data_member_list,
@@ -51,14 +55,42 @@ bool interpreter::read_stream(std::vector<data_member>& data_member_list,
 #endif
 		if (!(ss >> command)) // Empty line, just continue.
 			continue;
+		if (command == list_variables_command) {
+			try {
+				for (auto member : data_member_list) {
+					out << member.name << ": " << member.get_type()
+						<< std::endl;
+				}
+			}
+			catch (...) {
+				errors::dispatcher(err);
+			}
+			continue;
+		} 
+		if (command == help_command) {
+			out << "\nCommands:\n" <<
+				"list\ncreate\nset\nget\nexport\nimport\nexit\n" << 
+				std::endl;
+			continue;
+		}
 		if (command.substr(0, 2) == window_command) {
-			variables::set_window (
-				ss.str().substr(2, ss.str().length() - 4));
+			try {
+				variables::set_window(
+					ss.str().substr(2, ss.str().length() - 4));
+			}
+			catch (...) {
+				errors::dispatcher(err);
+			}
 			continue;
 		}
 		if (command.substr(0, 1) == new_executable_command) {
-			variables::set_executable(
-				ss.str().substr(1, ss.str().length() - 2));
+			try {
+				variables::set_executable(
+					ss.str().substr(1, ss.str().length() - 2));
+			}
+			catch (...) {
+				errors::dispatcher(err);
+			}
 			continue;
 		}
 		if (command == create_variable_command) {
